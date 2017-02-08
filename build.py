226f17a0097cc82d4ed0f747894a224a34fb71c3
Brandon <veryyes10@yahoo.com>
2017-02-08 18:53:10 -0500

    removed print in build

--- build.py
+++ build.py
@@ -8,8 +8,6 @@ contents = f.read()
 
 contents = contents.replace("<script type=\"text/javascript\" src=\"/static/js/main", "<script type=\"text/javascript\" src=\"./static/js/main")
 
-print(contents)
-
 f.close()
 
 f = open("./build/index.html", "w");
@@ -17,4 +15,4 @@ f = open("./build/index.html", "w");
 f.write(contents)
 f.close()
 
-
+print("Building Done")
