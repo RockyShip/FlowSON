aab3b8c0db506327ea94caffb031a5dae9fe8a45
Brandon <veryyes10@yahoo.com>
2017-02-09 23:45:18 -0500

    working boiler plate code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './containers/App'
 
-//"use strict";
-
-class HelloMessage extends React.Component{
-	render(){
-		return <div>Hello {this.props.name}</div>;
-	}
-}
-
-//window.onload = function(){
-	ReactDOM.render(<HelloMessage name="John" />, document.getElementById("container"));
-//}
+ReactDOM.render(
+	<App/>,
+	document.getElementById('container')
+)
